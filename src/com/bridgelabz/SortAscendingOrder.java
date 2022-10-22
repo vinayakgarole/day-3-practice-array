732115b5474e75e75558db4b3ea6890c7446a3b8
vinayakgarole <vinayakgarole10@gmail.com>
2022-10-22 17:12:21 +0530

    added: Sorted ascending elements in array

---
+++ src/com/bridgelabz/SortAscendingOrder.java
@@ -0,0 +1,35 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class SortAscendingOrder {
+    public static void main(String[] args) {
+
+        int arr[] = new int[5];
+        int temp;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Elements in Array: ");
+
+        for (int i=0; i<arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i=0; i<arr.length; i++) {
+
+            for (int j=i+1; j<arr.length; j++) {
+
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("Sort Ascending order Elements:");
+
+        for (int i=0; i<arr.length; i++) {
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
